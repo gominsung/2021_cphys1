1e2e5f6c5a33ae1adf9c8d1a65dcbfd22f8de990
gominsung <77944441+gominsung@users.noreply.github.com>
2021-03-12 19:05:02 +0900

    Update 로티스틱맵.py

--- 로티스틱맵.py
+++ 로티스틱맵.py
@@ -1,10 +1,11 @@
  # 숙제
 def homework(r,f_0):               
     a=0
+    c=f_0
     for i in range(1,11):
         a=a+1
         f=r*f_0*(1-f_0)
-        b="r=%f,n=%d일 경우의 함수값: %f" %(r,a,f)
+        b="r=%f,f_0=%f,n=%d일 경우의 함수값: %f" %(r,c,a,f)
         print(b)
         f_0=f
 homework(0.5,0.5)
