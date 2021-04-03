f7b5694f7e8cdf0ab07a1f72c2ad7b0446fd9cda
gominsung <77944441+gominsung@users.noreply.github.com>
2021-04-03 23:12:36 +0900

    1

--- hw4_1.py
+++ hw4_1.py
@@ -4,56 +4,64 @@ from scipy import integrate
 import math as m
 
 #Error Function
-def exp(t):
-    a=np.exp((t^2))
-    a=a^(-1)
-    return a
+def exp1(t):
+    a=-1*(t**2)
+    b=np.exp(a)
+    return b
+
 def errf(x):
-    a=integrate.quad(exp,0,x)[0]
-    return (2*a)/((np.pi)^0.5)    
+    a=integrate.quad(exp1,0,x)[0]
+    return (2*a)/((np.pi)**0.5)    
 
 def taylor(n,x):
     a=np.arange(0,n+1,1)
     f=0
     for i in a:
-        a_n1=((-1)^i)*x^(2*i+1)
-        a_n2=(2*i+1)*m.factorial(i)
-        an=a_n1/a_n2
+        an=(((-1)**i)*x**(2*i+1))/((2*i+1)*m.factorial(i))
         f=f+an
-    return (2/(np.pi))*f    
+    return (2/np.pi**(1/2))*f    
 
 x=np.arange(-2.5,2.5,0.01)
 b=[]
 c1=[]
 c2=[]
 c3=[]
+c4=[]
+
 for i in x:
     b.append(errf(i))
-plt.subplot(411)
-plt.plot(x,b,label="errof function")   
-plt.xlabel("x")
-plt.legend(loc="upper right") 
 
-plt.subplot(412)
 for i in x:
     c1.append(taylor(1,i))
-plt.plot(x,c1,label="errof function's Talor expension n=1")   
-plt.xlabel("x")
-plt.legend(loc="upper right")    
 
-plt.subplot(413)
 for i in x:
-    c2.append(taylor(8,i))
-plt.plot(x,c2,label="errof function's Talor expension n=8")   
-plt.xlabel("x")
-plt.legend(loc="upper right") 
+    c2.append(taylor(5,i))
 
-plt.subplot(413)
 for i in x:
-    c3.append(taylor(15,i))
-plt.plot(x,c3,label="errof function's Talor expension n=8")   
+    c3.append(taylor(10,i))
+
+for i in x:
+    c4.append(taylor(30,i))
+
+
+
+
+
+plt.title("Taylor approximation of Error Function")
+plt.plot(x,b,color="black",label="Error Function")
+plt.plot(x,c1,color="red",label="Taylor approximation of Error Function at n=1")
+plt.legend(loc="upper left",fontsize=7)
+
+
+
+plt.plot(x,c2,color="blue",label="Taylor approximation of Error Function at n=5")
+plt.legend(loc="upper left",fontsize=7)
+
+
+
+
+plt.plot(x,c3,color="green",label="Taylor approximation of Error Function at n=10")
+plt.legend(loc="upper left",fontsize=7)
+plt.ylim(-1.5,1.5)
 plt.xlabel("x")
-plt.legend(loc="upper right") 
 plt.show()
-
-
