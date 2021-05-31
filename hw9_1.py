3462049efcd2746bbbecacbf1b5cd5399f91958d
unknown <gominsung123@naver.com>
2021-05-31 14:35:21 +0900

    123

--- hw9_1.py
+++ hw9_1.py
@@ -2,34 +2,47 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt 
 
-def f(x): 
-    return x**2-np.cos(10*x)+1 # 에너지
-z=np.linspace(-2,2,100)
-mcs=10
-def mc(b,x):
-    for t in range(mcs):
-        newx=x+0.1*random.randint(-10,10) # 가우시안 랜덤 variable
-        DE=f(newx)-f(x)
-        if DE<0:
-            x=newx # 상태 바꾸기
-        elif random.random()<np.exp(-b*DE):
-            x=newx
-    return x
 
-x, Tmax=1.5, 10
-traj=[x]
-y = [f(x)]
-co = [0]
 
-for i in range(50):
-    T=Tmax*np.exp(-i/10) # cooling  
-    x=mc(1/T,x)
+
+x=[0, 10, 9, 5, 10, 5, 4, 2, 8, 9]
+y=[10, 0, 6, 8, 4, 3, 2, 7, 3, 2]
+
+def dist(x,y):
+    z=0
+    for i in range(9):
+        z+=np.sqrt((x[i+1]-x[i])**2+(y[i+1]-y[i])**2)
+    return z
+
+def de(i,j,x,y):
+    nx, ny = x[:], y[:]
+    nx[i], nx[j] = nx[j], nx[i]
+    ny[i], ny[j] = ny[j], ny[i]
+    return dist(nx,ny)-dist(x,y)
+
+
+def swap(x,y,b):
+    i=random.randint(0,9)
+    j=random.randint(0,9)
+    delta=de(i,j,x,y)
+    if(delta<0):
+        x[i], x[j] = x[j], x[i]
+        y[i], y[j] = y[j], y[i]
+    else:
+        if(random.random()<np.exp(-b*delta)):
+            x[i], x[j] = x[j], x[i]
+            y[i], y[j] = y[j], y[i]
+    return x,y    
+
+Tmax=20
+tau=10000
+for t in range(1000000):
+    T=Tmax*np.exp(-t/tau)
+    b=1/T
+    swap(x,y,b)
     
-    traj=np.append(traj,x)
-    y=np.append(y,f(x))
-    co=np.append(co,i+1)
-
-plt.plot(z,f(z))
-plt.scatter(traj,y,c=co,cmap='Greys')
-plt.plot(traj[-1],y[-1],'o')    
+plt.plot(x,y,'o')
+plt.plot(x,y)
 plt.show()
+print("시뮬레이티드 어닐링 방법으로 추론한 최소거리는 %s 이다." %dist(x,y))
+
