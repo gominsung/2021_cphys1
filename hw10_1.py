3cfbc6e670ef56f1afa42d2630890a3ecc1b86fc
unknown <gominsung123@naver.com>
2021-06-07 15:51:31 +0900

    1123
    123

---
+++ hw10_1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sympy
+from sympy import Symbol
+from sympy import *
+
+x= sympy.symbols("x")
+a= sympy.symbols("a")
+
+print(sympy.integrate(sympy.exp(-a*x**2),(x,-sympy.oo,sympy.oo)))
+print(sympy.integrate(x*sympy.exp(-a*x**2),(x,-sympy.oo,sympy.oo)))
+print(sympy.integrate(x**2*sympy.exp(-a*x**2),(x,-sympy.oo,sympy.oo)))
+print(sympy.integrate(x**4*sympy.exp(-a*x**2),(x,-sympy.oo,sympy.oo)))
+
+
+t = symbols('t')
+x = Function('x') 
+print(dsolve(Eq(x(t).diff(t),-a*x(t))))
